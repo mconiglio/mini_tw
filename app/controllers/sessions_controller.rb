2a9ed1a8d87793626f81e474d87352b613489cf8
Mauricio Coniglio <mconiglio.782@gmail.com>
2015-10-07 13:51:52 -0300

    Terminado listado de usuarios y operaciones CRUD

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   	if user && user.authenticate(params[:session][:password])
   		log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-  		redirect_to user
+  		redirect_back_or user #Si se queria ir a otra pagina, luego de loguearse lleva a esa
   	else
   		flash.now[:danger] = 'Invalid email/password combination'
   		render 'new'
